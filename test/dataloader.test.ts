3efad95d79977b2359a9c5808d723df0edb7c205
Corentin Leruth <corentin.leruth@gmail.com>
2024-03-03 17:35:58 +0100

    more assertions

--- test/dataloader.test.ts
+++ test/dataloader.test.ts
@@ -1,14 +1,19 @@
-import { expect, test, describe } from 'bun:test';
+import { expect, test, describe, spyOn } from 'bun:test';
 import { makeLoader } from '~/dataloader';
-import { getUserByIds } from '~/db';
+import * as db from '~/db';
 
 describe('dataloader', () => {
 	test('getUserByIds', async () => {
-		const userLoader = makeLoader((ids) => getUserByIds(ids));
+		const spy = spyOn(db, 'getUserByIds');
+
+		const userLoader = makeLoader((ids) => db.getUserByIds(ids));
 
 		const ids = [1, 2, 3, 4, 5];
 
 		const result = await Promise.all(ids.map((id) => userLoader.load(id)));
+
+		expect(spy).toHaveBeenCalledTimes(1);
+		expect(spy).toHaveBeenCalledWith(ids);
 
 		expect(result).toEqual(
 			ids.map((id) => ({
